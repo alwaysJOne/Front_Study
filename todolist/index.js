4d68bb9b48e89f79b2950790bf16ed1bc69de83d
alwaysJOne <alwaysjone@naver.com>
2023-10-10 14:53:22 +0900

    edit js

--- todolist/index.js
+++ todolist/index.js
@@ -1,17 +1,70 @@
+const todoList = [];
+
 function addTodo(ev){
-    console.log(ev.keyCode)
-    //내가 작성한 일정명 가지고오기
+
+    //요소만들기
+    //내가 입력한 값 가지고오기
     const todoTitle = document.getElementById('search-input').value;
-    if (todoTitle.trim() === "")
-        return;
+
+    //데이터 만들어주기
+    let data = {
+        title : todoTitle,
+        isDone : false,
+        date : new Date().getTime()
+    }
+
+    todoList.push(data);
+
+    const todoNode = document.createElement('div'); // <div></div>
+    todoNode.innerText = data.title;
+    toDoListView.appendChild(todoNode);
+    
+    const removeBtn = document.createElement('button'); // <button><i class='fa-solid fa-xmark'></i></button>
+    removeBtn.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+    todoNode.appendChild(removeBtn); // <div>밥먹기<button><i class='fa-solid fa-xmark'></i></button></div>
+
+
+    toDoListView.innerHTML += "<div onclick='isSuccess(this)'>" + todoTitle + 
+    "<button onclick='removeTodo(this)' class='remove-btn'><i class='fa-solid fa-xmark'></i></button>"
+    +"</div>";
+
+    //요소 붙여주기
+    //붙일 공간 가지고오기
+
+
+
+
+
+
+
+    //추가할 일정요소 만들기
+    function getTodoNode(data){
+        toDoListView.innerHTML += "<div onclick='isSuccess(this)'>" + todoTitle + 
+        "<button onclick='removeTodo(this)' class='remove-btn'><i class='fa-solid fa-xmark'></i></button>"
+        +"</div>";
+
+    }
+   
 
     //엔터키를 입력했다면 해당코드 실행
     if (!ev.keyCode || ev.keyCode === 13) {
+        //데이터 저장
+        let data = {
+            title : todoTitle,
+            isDone : false,
+            date : new Date().getTime()
+        };
+        
+        todoList.push(data);
+
         //일정을 보여주는 영역
         const toDoListView = document.querySelector('#todo-list-view');
+        //추가할 일정 요소
+        const toDoNode = getTodoNode(data);
+        toDoListView.appendChild(toDoNode);
 
         //추가할 일정요소 만들기
-        toDoListView.innerHTML += "<div>" + todoTitle + "<button onclick='removeTodo(this)' class='remove-btn'><i class='fa-solid fa-xmark'></i></button></div>";
+      
 
         //추가한 일정 input에서 지워주기
         document.getElementById('search-input').value = "";
@@ -20,4 +73,13 @@ function addTodo(ev){
 
 function removeTodo(button){
     button.parentNode.remove();
+}
+
+function isSuccess(todo){
+    // classList => 해당 요소가 가지고 있는 클레스들을 배열로 반환
+    if (todo.classList.length === 0) {
+        todo.className = 'done';
+    } else {
+        todo.className = '';
+    }
 }

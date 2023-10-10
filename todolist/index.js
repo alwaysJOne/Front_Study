781c6523025f20b09f14557608622497bc792af0
alwaysJOne <alwaysjone@naver.com>
2023-10-10 15:27:35 +0900

    js add newTodoList

--- todolist/index.js
+++ todolist/index.js
@@ -2,84 +2,49 @@ const todoList = [];
 
 function addTodo(ev){
 
-    //요소만들기
-    //내가 입력한 값 가지고오기
-    const todoTitle = document.getElementById('search-input').value;
-
-    //데이터 만들어주기
-    let data = {
-        title : todoTitle,
-        isDone : false,
-        date : new Date().getTime()
-    }
-
-    todoList.push(data);
-
-    const todoNode = document.createElement('div'); // <div></div>
-    todoNode.innerText = data.title;
-    toDoListView.appendChild(todoNode);
-    
-    const removeBtn = document.createElement('button'); // <button><i class='fa-solid fa-xmark'></i></button>
-    removeBtn.innerHTML = "<i class='fa-solid fa-xmark'></i>";
-    todoNode.appendChild(removeBtn); // <div>밥먹기<button><i class='fa-solid fa-xmark'></i></button></div>
-
-
-    toDoListView.innerHTML += "<div onclick='isSuccess(this)'>" + todoTitle + 
-    "<button onclick='removeTodo(this)' class='remove-btn'><i class='fa-solid fa-xmark'></i></button>"
-    +"</div>";
-
-    //요소 붙여주기
-    //붙일 공간 가지고오기
-
-
-
-
-
-
+    if (document.getElementById('search-input').value.trim() === '')
+        return;
 
     //추가할 일정요소 만들기
-    function getTodoNode(data){
-        toDoListView.innerHTML += "<div onclick='isSuccess(this)'>" + todoTitle + 
-        "<button onclick='removeTodo(this)' class='remove-btn'><i class='fa-solid fa-xmark'></i></button>"
-        +"</div>";
+    function getTodoNode(){
+         //요소만들기
+        //내가 입력한 값 가지고오기
+        const todoTitle = document.getElementById('search-input').value;
 
+        //todo요소만들고 붙여주기
+        const todoNode = document.createElement('div'); // <div>title</div>
+        todoNode.innerText = todoTitle;
+        todoNode.onclick = function(){
+            todoNode.isDone = !todoNode.isDone;
+            if (todoNode.isDone) {
+                this.className = 'done';
+            } else {
+                this.className = '';
+            }
+        }
+        
+        //요소삭제버튼 만들고 붙여주기
+        const removeBtn = document.createElement('button'); // <button><i class='fa-solid fa-xmark'></i></button>
+        removeBtn.className = 'remove-btn';
+        removeBtn.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+        todoNode.appendChild(removeBtn); // <div>밥먹기<button><i class='fa-solid fa-xmark'></i></button></div>
+        removeBtn.onclick = function(){
+            this.parentNode.remove();
+        }
+
+        document.getElementById('search-input').value = '';
+
+        return todoNode;
     }
-   
 
     //엔터키를 입력했다면 해당코드 실행
     if (!ev.keyCode || ev.keyCode === 13) {
-        //데이터 저장
-        let data = {
-            title : todoTitle,
-            isDone : false,
-            date : new Date().getTime()
-        };
-        
-        todoList.push(data);
-
-        //일정을 보여주는 영역
+        //일정을 보여주는 영역 가져오기
         const toDoListView = document.querySelector('#todo-list-view');
-        //추가할 일정 요소
-        const toDoNode = getTodoNode(data);
-        toDoListView.appendChild(toDoNode);
-
-        //추가할 일정요소 만들기
-      
-
-        //추가한 일정 input에서 지워주기
-        document.getElementById('search-input').value = "";
+        // 요소 생성하는 함수호출
+        const todoNode = getTodoNode();
+        toDoListView.appendChild(todoNode);
     }
-}
 
-function removeTodo(button){
-    button.parentNode.remove();
-}
 
-function isSuccess(todo){
-    // classList => 해당 요소가 가지고 있는 클레스들을 배열로 반환
-    if (todo.classList.length === 0) {
-        todo.className = 'done';
-    } else {
-        todo.className = '';
-    }
 }
